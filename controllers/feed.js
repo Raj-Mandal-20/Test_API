2f2cba9eb7f375eead2cc2f4c3ab1e7b48b45203
Raj Mandal <99534700+Raj-Mandal-20@users.noreply.github.com>
2024-02-17 17:34:09 +0530

    Revert "Revert "[FIX] :  Added Authentication on our API & Docker Setup""

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,21 +1,26 @@
+const Post = require("../model/post");
 const { validationResult } = require("express-validator");
 
-exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    message: "You are Fetching Details Successfully",
-    post: [
-      {
-        title: "Biriyani",
-        describe: "Bengali's Best Food",
-        prize: 300,
-      },
-    ],
-    createdAt: new Date(),
-    createdBy: "Raj Mandal",
-  });
+exports.getPosts = async (req, res, next) => {
+  try {
+    const resData = await Post.find({});
+    console.log(resData);
+  
+    res.status(200).json({
+      message: "You are Fetching Details Successfully",
+      post: resData,
+      createdAt: new Date(),
+      createdBy: "Raj Mandal",
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 };
 
-exports.createPost = (req, res, next) => {
+
+exports.createPost = async (req, res, next) => {
+  // console.log(req.body);
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -25,21 +30,37 @@ exports.createPost = (req, res, next) => {
     });
   }
 
-  const title = req.body.title;
-  const describe = req.body.describe;
-  const prize = req.body.prize;
-  const createdBy = req.body.createdBy;
+  if (!req.file) {
+    const error = new Error("No Image Provided");
+    error.statusCode = 422;
+    // console.log("file ERROR ☑");
+    // console.log(error);
+    throw error;
+  }
 
-  res.status(201).json({
-    message: "Post Created Successfully!",
-    post: [
-      {
-        title: title,
-        describe: describe,
-        prize: prize,
-      },
-    ],
-    createdAT: new Date(),
-    createdBy: createdBy,
-  });
+  try {
+    // console.log(req.file?.path);
+    console.log(req.file.path);
+    const title = req.body.title;
+    const describe = req.body.describe;
+    const imageUrl = req.file.path.replace("\\", "/");
+    const prize = req.body.prize;
+    // const createdBy = req.body.createdBy;
+
+    const post = new Post({
+      title: title,
+      describe: describe,
+      imageUrl: imageUrl,
+      prize: prize,
+    });
+
+    await post.save();
+
+    res.status(201).json({
+      message: "Post Created Successfully!",
+      createdAT: new Date(),
+    });
+  } catch (err) {
+    next(err);
+  }
 };
