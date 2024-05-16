ad87f0ce807a5b6650401e6678552d0e3195449e
Raj Mandal <usingrajcseinfo@gmail.com>
2024-05-17 01:19:03 +0530

    new change

--- routes/feed.js
+++ routes/feed.js
@@ -5,10 +5,10 @@ const { body } = require("express-validator");
 const isAuth = require("../middleware/is_auth");
 const feedController = require("../controllers/feed");
 
-router.get("/posts", isAuth, feedController.getPosts);
+router.get("/posts",/* isAuth,*/ feedController.getPosts);
 router.post(
   "/post",
-  isAuth,
+  // isAuth,
   [
     body("title").trim().isLength({
       min: 5,
